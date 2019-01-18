2566f537a078ec90aac6e94829ef9d8f8442f772
fabestr <f.estrabaud@gmail.com>
2019-01-18 12:50:46 +0100

    fuck

---
+++ J.S/Ardoise magique/main.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById('slate');
+
+var slate = new Slate(canvas, 1);
+
+console.log(slate);
+
+var canvas2 = document.getElementById('slate2');
+
+var slate2 = new Slate(canvas2, 2);
